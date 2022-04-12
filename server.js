dc013edb5523712acf64078e0f5d94de7350e9ae
Luis <yomiguel112@gmail.com>
2022-04-12 17:23:44 -0500

    compiling models

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const moongose = require("mongoose");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
 app.use(express.json());
@@ -8,13 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(express.static("public"));
+
+moongose.connect(process.env.MONGO_URI);
+
+const Schema = mongoose.Schema;
+const usersDataSchema = new Schema({ user_name: String });
+const exerciseDataSchema = new Schema({
+  description: String,
+  duration: Number,
+  date: Date
+});
+
+const User = mongoose.model("user", usersDataSchema);
+cosnt Exercise = mongoose.model("exercise", exerciseDataSchema);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/users", (req, resp) => {
-
-});
+app.post("/api/users", (req, resp) => {});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
