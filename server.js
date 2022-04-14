48c28c80ee89d8bb5c2e01d1f0dd5328cabf61a8
Luis <yomiguel112@gmail.com>
2022-04-14 09:59:20 -0500

    second endpoint done

--- server.js
+++ server.js
@@ -3,32 +3,57 @@ const app = express();
 const cors = require("cors");
 const moongose = require("mongoose");
 const { default: mongoose } = require("mongoose");
+const response = require("express");
+const { handle } = require("express/lib/application");
 require("dotenv").config();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-moongose.connect(process.env.MONGO_URI);
-
-const Schema = mongoose.Schema;
-const usersDataSchema = new Schema({ user_name: String });
-const exerciseDataSchema = new Schema({
-  description: String,
-  duration: Number,
-  date: Date
+moongose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const Schema = mongoose.Schema;
+
+const usersDataSchema = new Schema({ username: String });
+const exerciseDataSchema = new Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date,
+});
+const logSchema = new Schema({ username: String, count: Number, log: Array });
+
 const User = mongoose.model("user", usersDataSchema);
-cosnt Exercise = mongoose.model("exercise", exerciseDataSchema);
+const Exercise = mongoose.model("exercise", exerciseDataSchema);
+const Log = mongoose.model("log", logSchema);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/users", (req, resp) => {});
+app.post("/api/users", async (req, res) => {
+  new User({ username: req.body.username }).save((error) => {
+    if (error) {
+      return handlerError(error);
+    }
+  });
+  const userSeeker = await User.findOne({ username: req.body.username });
+  res.json({ username: userSeeker.username, _id: userSeeker["_id"] });
+});
+
+app.get("/api/users", async (req, res) => {
+  const namePattern = /[\s\S]/;
+  const allUsers = await User.find({ username: namePattern });
+  const usersList = allUsers.map((user) => {
+    return { username: user.username, _id: user["_id"] };
+  });
+  res.send(usersList);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
